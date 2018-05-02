b8a4711a259b3616f6f4b21b4d0c1fbcbeea1f8f
David Hull <david.hull1@utexas.edu>
2018-05-02 14:12:33 -0500

    Created shopping cart components and reducers

--- reactRedux/redux-react/src/reducers/cart.js
+++ reactRedux/redux-react/src/reducers/cart.js
@@ -1,3 +1,10 @@
-// export default(state=[], payload) => {
-//     switch(payload.type)()
-// }
+
+// src/reducers/cart.js
+export default(state = [], payload) => {
+    switch (payload.type) {
+        case 'add':
+            return [...state, payload.item];
+        default:
+            return state;
+    }
+};
--- reactRedux/redux-react/src/reducers/index.js
+++ reactRedux/redux-react/src/reducers/index.js
@@ -1,6 +1,8 @@
+// this is the root reducer
 import cart from './cart';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
     cart: cart
 });
+export default rootReducer;
